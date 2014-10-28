d7aa0e966d6040b393f5e74aead73ade13411d98
volodg <volodg@mail.ru>
2014-10-28 21:27:46 +0200

    add dreambook csv import

--- migrationTools/Examples.hs
+++ migrationTools/Examples.hs
@@ -1,42 +1,48 @@
+{-# LANGUAGE DataKinds #-}
+
 module Main where
 
+-- Dream Book
+-- https://docs.google.com/spreadsheets/d/1QEVJvwrRpGvuvVh2XAKaNQWem5HwYh7q9D7Cs9-2lFE/edit#gid=0
+
 import Control.Monad.Cont
+import Data.Conduit
+import Control.Monad.Trans.Resource
+import qualified Data.Conduit.List as CL
+import qualified Data.Conduit.Binary as CB
+import qualified Data.ByteString as B
+import Data.ByteString.Char8 as BC hiding (writeFile, readFile)
+import Control.Monad.Trans.State
+import Model
+import qualified Data.Text as T
+import Data.List.Split
+import Debug.Trace
 
-ex2 = do
-    a <- return 1
-    b <- cont (\fred -> fred 10)
-    return $ a+b
+sink :: MonadResource m => Sink (Int, Dream) m () -- consumes a stream of Strings, no result
+sink = CL.mapM_ $ \x -> do
+    (_releaseKey, _resource) <- allocate (return ()) (\_ -> return ())
+    liftIO $ Prelude.putStrLn $ show x
+    return ()
 
-test2 = runCont ex2 show
+strLineToDream :: B.ByteString -> Int -> (Int, Dream)
+strLineToDream line fieldID = (fieldID, Dream word context (T.pack "ru") dreamType) where
+    lineStr = BC.unpack line
+    [word, context, dreamType, _] = Prelude.map T.pack $ splitOn "," lineStr
+--    word = 
 
-ex3 = do
-    a <- return 1
-    b <- cont (\fred -> "escape")
-    return $ a+b
+numLines :: MonadResource m => Int -> Conduit B.ByteString m (Int, Dream)
+numLines lineNum = do
+    lineOpt <- await
+    case lineOpt of
+        Just line ->
+            let next = numLines (lineNum + 1) in
+            if (lineNum /= 0)
+                then do
+                    yield $ strLineToDream line lineNum
+                    next
+                else
+                    next
+        _ -> return ()
 
-test3 = runCont ex3 show
-
-ex4 = do
-    a <- return 1
-    b <- cont (\fred -> fred 10 ++ fred 20)
-    return $ a+b
-
-test4 = runCont ex4 show
-
-ex7 = do
-    a <- return 1
-    b <- cont (\fred -> [10,20] >>= fred)
-    return $ a+b
-
-test7 = runCont ex7 (\x -> [x])
-
-i x = cont (\fred -> x >>= fred)
-
-run m = runCont m return
-
-test9 = run $ do
-    a <- i [1, 2]
-    b <- i [10,20]
-    return $ a+b
-
-main = print test9
+main = do
+    runResourceT $ CB.sourceFile "DreamBook-ru.csv" $$ CB.lines =$ (numLines 0) =$ sink
