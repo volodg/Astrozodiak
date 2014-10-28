357c29d87b9bc195926ed53c51d35c7c899d577d
volodg <volodg@mail.ru>
2014-10-28 23:27:08 +0200

    import from csv

--- migrationTools/Examples.hs
+++ migrationTools/Examples.hs
@@ -17,6 +17,10 @@ import Model
 import qualified Data.Text as T
 import Data.List.Split
 import Debug.Trace
+import qualified Data.Map as DM
+
+-- import Data.Conduit.Binary
+import Data.CSV.Conduit
 
 sink :: MonadResource m => Sink (Int, Dream) m () -- consumes a stream of Strings, no result
 sink = CL.mapM_ $ \x -> do
@@ -24,25 +28,29 @@ sink = CL.mapM_ $ \x -> do
     liftIO $ Prelude.putStrLn $ show x
     return ()
 
-strLineToDream :: B.ByteString -> Int -> (Int, Dream)
-strLineToDream line fieldID = (fieldID, Dream word context (T.pack "ru") dreamType) where
-    lineStr = BC.unpack line
-    [word, context, dreamType, _] = Prelude.map T.pack $ splitOn "," lineStr
---    word = 
+csvRawToDream :: MapRow T.Text -> Int -> Maybe (Int, Dream)
+csvRawToDream raw fieldID = do
+    context <- rawLookup "context"
+    rawType <- rawLookup "type"
+    word    <- rawLookup "word"
+    return (fieldID, Dream word context (T.pack "ru") rawType) where
+        rawLookup key = DM.lookup (T.pack key) raw
 
-numLines :: MonadResource m => Int -> Conduit B.ByteString m (Int, Dream)
-numLines lineNum = do
+toDream :: MonadResource m => Int -> Conduit (MapRow T.Text) m (Int, Dream)
+toDream lineNum = do
     lineOpt <- await
     case lineOpt of
-        Just line ->
-            let next = numLines (lineNum + 1) in
-            if (lineNum /= 0)
-                then do
-                    yield $ strLineToDream line lineNum
-                    next
-                else
-                    next
+        Just line -> do
+            case csvRawToDream line lineNum of
+                Just el -> yield $ el
+                Nothing -> liftIO $ Prelude.putStrLn $ "can not parse line: " ++ show line
+            toDream (lineNum + 1)
         _ -> return ()
 
-main = do
-    runResourceT $ CB.sourceFile "DreamBook-ru.csv" $$ CB.lines =$ (numLines 0) =$ sink
+test = runResourceT $
+  CB.sourceFile "DreamBook-ru.csv" $=
+  intoCSV defCSVSettings $=
+  (toDream 1) $$
+  sink
+
+main = test
