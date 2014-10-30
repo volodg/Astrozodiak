9529c208795afae7d5704321b7d535e87d0d5803
volodg <volodg@mail.ru>
2014-10-30 21:06:44 +0200

    push data to DB

---
+++ migrationTools/ScvToDream.hs
@@ -0,0 +1,83 @@
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Main where
+
+-- Dream Book
+-- https://docs.google.com/spreadsheets/d/1QEVJvwrRpGvuvVh2XAKaNQWem5HwYh7q9D7Cs9-2lFE/edit#gid=0
+
+import Control.Monad.Trans.Resource --(MonadResource, allocate)
+import Control.Monad.Trans.Class
+import Control.Monad.IO.Class --(liftIO)
+import qualified Data.Conduit.Binary as CB
+import qualified Data.Text as T
+import qualified Data.Map as DM
+import Data.CSV.Conduit
+import Data.Conduit
+import Control.Monad.Logger (runStdoutLoggingT)
+import Database.Persist.Postgresql
+import Data.Pool (destroyAllResources)
+import Model
+import Database.Persist
+import qualified Database.Esqueleto as E
+import GHC.Int
+
+connStr = "host=localhost dbname=Astrozodiak user=vladimirgorbenko password=H4d3yl8x port=5432"
+
+--forall a. 
+openConn :: IO ConnectionPool
+openConn = do
+    putStrLn "start"
+    pool <- runStdoutLoggingT $ do
+        pool <- createPostgresqlPool connStr 10
+        return pool
+    return pool
+
+closeConn :: ConnectionPool -> IO ()
+closeConn pool = do
+    putStrLn "stop"
+    destroyAllResources pool
+
+sink :: MonadResource m => Sink (Int64, Dream) m () -- consumes a stream of Strings, no result
+sink = do
+    (_releaseKey, pool) <- allocate openConn closeConn
+    awaitForever $ \dreamWithID -> do
+        let dreamIDField = fst dreamWithID
+        let dreamVal     = snd dreamWithID
+
+        liftIO $ do
+            flip runSqlPool pool $ repsert (toSqlKey dreamIDField) dreamVal
+    -- TODO remove items
+
+    -- liftIO $ do
+    --     flip runSqlPool pool $ do
+    --         delete lastKey
+
+    return ()
+
+csvRawToDream :: MapRow T.Text -> Int64 -> Maybe (Int64, Dream)
+csvRawToDream raw fieldID = do
+    context <- rawLookup "context"
+    rawType <- rawLookup "type"
+    word    <- rawLookup "word"
+    return (fieldID, Dream word context (T.pack "ru") rawType) where
+        rawLookup key = DM.lookup (T.pack key) raw
+
+toDream :: MonadResource m => Int64 -> Conduit (MapRow T.Text) m (Int64, Dream)
+toDream lineNum = do
+    lineOpt <- await
+    case lineOpt of
+        Just line -> do
+            case csvRawToDream line lineNum of
+                Just el -> yield $ el
+                Nothing -> liftIO $ putStrLn $ "can not parse line: " ++ show line
+            toDream (lineNum + 1)
+        _ -> return ()
+
+csvToDB = runResourceT $
+  CB.sourceFile "DreamBook-ru.csv" $=
+  intoCSV defCSVSettings $=
+  (toDream 1) $$
+  sink
+
+main = csvToDB
